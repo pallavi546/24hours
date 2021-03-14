0f49018bab45afef9fb00f99990ee42f40c10b24
pallavi546 <74195532+pallavi546@users.noreply.github.com>
2021-03-14 17:18:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ const Body = Matter.Body;
 var wall1,wall2,wall3;
 var ground;
 var ball;
+var call;
 function preload()
 {
 	
@@ -21,14 +22,15 @@ function setup() {
    
 	engine = Engine.create();
 	world = engine.world;
-    
+    // createSprite();
 	//Create the Bodies Here.
-	wall1=new Wall(600,650,180,20);
-	wall2=new Wall(695,610,20,100)
-	wall3=new Wall(505,610,20,100)
-	ball=new Paper(400,400,50)
+	wall1=new Wall(510,650,180,20);
+	wall2=new Wall(695,610,20,120)
+	wall3=new Wall(505,610,20,120)
+	ball=new Paper(120,600,20);
 	
-    
+	
+   
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
 	World.add(world, ground);
 
@@ -38,9 +40,10 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
   background(0);
 drawSprites();
+
+
  
   wall1.display();
   wall2.display();
@@ -48,6 +51,14 @@ drawSprites();
   ball.display();
   
   
+  
+}
+function keyPressed(){
+  if (keyCode===38){
+Matter.Body.applyForce(ball.body,ball.body.position,{x:60,y:-55})
+	
+  }
+
 }
 
 
