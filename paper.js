27e5ec46700a9a5fd9f60a6fa1a498f6e21c10f6
pallavi546 <74195532+pallavi546@users.noreply.github.com>
2021-03-14 17:16:54 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,28 +1,25 @@
 class Paper {
     constructor(x, y,radius) {
+    this.radius=radius;
 
       var options={
-        isStatic:true,
+        isStatic:false,
         restitution:0.3,
         friction:0.5,
         density:1.2
         
       }
       
-     this.body= Matter.Bodies.circle(x,y,radius);
-      
-      
-      
-      
+     this.body=Bodies.circle(x,y,radius,options);
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
       
-      fill("pink");
-      this.radius=radius
-      circle(x,y,this.radius);
-      ellipse(CENTER);
+      fill("red");
+      ellipseMode(RADIUS)
+      circle(pos.x,pos.y,this.radius);
+      
     }
   };
   
